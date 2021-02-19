6e814748524e2e01fe81a772373d3117776919d9
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-02-19 15:22:55 +0900

    HIROSHI

--- Eureka_Vtest.ts
+++ Eureka_Vtest.ts
@@ -92,7 +92,7 @@ enum LED_wait {
     }
 
 
-//% color="#32cd32" weight=100 block="ふく合ﾕﾆｯﾄ2.71"
+//% color="#32cd32" weight=100 block="ふく合ﾕﾆｯﾄtest"
 
 namespace eureka_blocks {
 
@@ -364,6 +364,26 @@ namespace eureka_blocks_soro {
         break; 
         }
   }
+
+
+  namespace stem {
+    /**
+     * TFW-TP2の温度[℃]を返します。
+     * @param format number format, eg: OutputNumberFormat.INTEGER
+     */
+    //% blockId = TP2_getTemperature
+    //% block="Temperature[degC] (TP2) || %format"
+    //% group="TP2"
+    //% weight=100
+    export function TP2_getTemperature(format: OutputNumberFormat = OutputNumberFormat.INTEGER): number {
+        if (format === OutputNumberFormat.INTEGER) {
+            return Math.round(DS18B20.Temperature()/100.0);
+        }
+        return DS18B20.Temperature()/100.0;
+    }
+}
+
+
 
 
   //% color="#6041f1"  weight=60 blockId=eureka_L9110 block="ﾓｰﾀｰﾌｧﾝL |%mode| |%pin|" group="4_ユーレカ装置"
